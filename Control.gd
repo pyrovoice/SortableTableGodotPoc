fffdb51ec396fa9a4ef9b1dbb144a77c8f8178e1
mgrazzin <mgrazzin@sport1.de>
2024-01-11 11:33:52 +0100

    renaming stuff

--- Control.gd
+++ Control.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var grid_container = $MarginContainer/GridContainer
 const inputData = ["String 1", "String 2", "String 3", "String 4", "String 5", "String 6"]
 const skillLine = preload("res://SkillLine.tscn")
-var draggedComponent = null
+var draggedComponent: Control = null
 
 func _ready():
 	for child in grid_container.get_children():
@@ -14,18 +14,21 @@ func _ready():
 		newLine.init(input)
 		newLine.buttonAction.connect(func(b):onLineButtonPressed(newLine, b))
 		newLine.mouseAction.connect(func(entered): onMouseEnterElement(newLine, entered))
-		newLine.name = "Line " + str(newLine.get_index())
+		newLine.name = input
 
 func onLineButtonPressed(child, isPressed: bool):
 	if isPressed:
 		draggedComponent = child
+		draggedComponent.z_index = 1
 	else:
+		draggedComponent.z_index = 0
 		draggedComponent = null
 
 func onMouseEnterElement(enteredElement, isMouseEnter: bool):
 	if draggedComponent && isMouseEnter:
+		print("Entered " + enteredElement.name)
 		grid_container.move_child(draggedComponent, enteredElement.get_index())
 
 func _input(event):
 	if event is InputEventMouseMotion && draggedComponent:
-		draggedComponent.position.y += (event as InputEventMouseMotion).relative.y
+		draggedComponent.position.y = get_global_mouse_position().y - draggedComponent.size.y/2
